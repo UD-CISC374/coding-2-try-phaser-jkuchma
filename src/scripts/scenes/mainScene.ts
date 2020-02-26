3e50ab8a9759af0d09451383135d6e3896f326d0
jkuchma <jkuchma@udel.edu>
2020-02-26 10:25:07 -0500

    Added images

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,6 +2,7 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -9,6 +10,8 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.exampleObject = new ExampleObject(this, 0, 0);
+    this.background = this.add.image(0,0,"background");
+    this.background.setOrigin(0,0);
   }
 
   update() {
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,9 +4,14 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "assets/images/background.png");
+    this.load.image ("ring", "assets/image/ring.png");
+    this.load.image("chilidog", "assets/images/Chili_Dog");
   }
 
   create() {
+    this.add.text(20,20,"Loading game...");
     this.scene.start('MainScene');
+
   }
 }
