c43e4fd0abaa205c26334ded7cb15eee71573019
jkuchma <jkuchma@udel.edu>
2020-02-27 21:12:36 -0500

    Added some more

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -5,13 +5,20 @@ export default class PreloadScene extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "assets/images/background.png");
-    this.load.image ("ring", "assets/image/ring.png");
-    this.load.image("chilidog", "assets/images/Chili_Dog");
+    this.load.image("player", "assets/spritesheets/player.png");
+    this.load.image("ship", "assets/spritesheets/ship.png");
+    this.load.image("ship2", "assets/spritesheets/ship2.png");
+    this.load.image("ship3", "assets/spritesheets/ship3.png");
+    this.load.image("explosion", "assets/spritesheets/explosions.png");
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png");
   }
 
   create() {
     this.add.text(20,20,"Loading game...");
     this.scene.start('MainScene');
 
+    
   }
+
+
 }
