32157d3895e2d5be3ffeb1e5505c30eb1965f303
jkuchma <jkuchma@udel.edu>
2020-02-27 21:15:06 -0500

    Added stuff

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -1,12 +1,18 @@
 export default class PreloadScene extends Phaser.Scene {
+  background: Phaser.GameObjects.Image;
   constructor() {
     super({ key: 'PreloadScene' });
   }
 
   preload() {
+    this.load.image("background","assets/images/background.png");
   }
 
   create() {
     this.scene.start('MainScene');
+    this.scene.start("playGame");
+
+    this.background = this.add.image(0,0,"background");
+    this.background.setOrigin(0,0);
   }
 }
